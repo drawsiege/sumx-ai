f3adfd81cd203f468be77d9374a3808d8d697d4b
drawsiege <yusukebalaoy@gmail.com>
2023-05-05 08:14:31 +0800

    url changed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
         <header className='w-full flex justify-center items-center flex-col'>
             <nav className='flex justify-between items-center w-full mb-10 pt-3'>
                 <img src={logo} alt='sumz_logo' className='w-28 object-contain' />
-                <button type='button' onClick={() => window.open('https://github.com/drawsiege/')} className='black_btn'>
+                <button type='button' onClick={() => window.open('https://github.com/drawsiege/sumx-ai')} className='black_btn'>
                     Github
                 </button>
             </nav>
